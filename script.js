b5842bcfb97821a108cf0cfab4c8207c44e07575
bws.ksi-DMRKT <bws.ksi@gmail.com>
2022-02-23 16:42:47 +0200

    edit .js draft

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-getObj()
+function getObj()
+
+// #2 task
 
 Country(title - название страны, capital - столица, population - население, area - площадь)
 
 getCountry( getObj() )
 
+let Country1 = new Country("France", "Paris", 1854565, 45054851)
+let Country2 = new Country("Italy", "Rome", 654654654, 465745)
+let Country3 = new Country("Germany", "Berlin", 654654654, 6545454)
+let Country4 = 
