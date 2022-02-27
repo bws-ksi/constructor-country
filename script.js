2b89b9aba1c1d9e2f84179e0eb2719bc6e3589d7
bws.ksi <bws.ksi@gmail.com>
2022-02-27 11:20:11 +0200

    eiting .js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 function getObj() {
-  return this;
-}
-
-getCoutry(){
   for (let key in Country){
-    console.log(key);
+    Country.getCountry[key];
   }
 }
-
 function Country(title, capital, population, area) {
   this.titleCountry = title;
   this.capitalCountry = capital;
   this.populationCountry = population;
   this.areaCountry = area;
-  this.formsOfGovernment = "Republic"
-  this.displayInfo = getCountry(); //??????
+  this.formsOfGovernment = "Republic";
+  this.getCountry = getObj();  
 };
 
+const france = new Country("France", "Paris", 1854565, 45054851)
+//console.table(france);
+ let italy = new Country("Italy", "Rome", 654654654, 465745)
+// let germany = new Country("Germany", "Berlin", 654654, 6545454)
 
-let france = new Country("France", "Paris", 1854565, 45054851)
-let italy = new Country("Italy", "Rome", 654654654, 465745)
-let germany = new Country("Germany", "Berlin", 654654654, 6545454)
 
-console.log(france);
-console.log(italy);
-console.log(germany);
+console.table(italy.getCountry);
+
+
+
+
+
+
+/*
+for (let key in Country){
+  console.log(key)
+}
+*/
